51b61e27e18aad2a3b0d1f322250f766c19f8cbe
Lavish Sharma <sharmalavish375@gmail.com>
2024-06-10 17:09:09 +0530

    lec-5/completed

--- script.js
+++ script.js
@@ -1,41 +1,50 @@
-var menu = document.querySelector(".nav i");
-var close = document.querySelector(".full i")
+function breakTheElement()
+{
+  var h1=document.querySelector("h1");
+  
+  var splittedtext=h1.textContent.split("");
 
-var tl = gsap.timeline();
+  var halflength=Math.floor(splittedtext.length/2);
 
-tl.to(".full",
+  
+  var clutter="";
+  
+  splittedtext.forEach(function(dets,ind)
   {
-    right: "0",
-    duration: 0.8
+    if(ind<halflength)
+      {
+        clutter+=`<span class="a"
+        >${dets}</span>`;
+      }
+
+      else
+      {
+        clutter+=`<span class="b"
+        >${dets}</span>`;
+      }
+  })
+  
+  h1.innerHTML=clutter;
+
+}
+
+breakTheElement();
+
+
+gsap.from("h1 .a",
+  {
+    y:70,
+    duration:0.6,
+    delay:0.4,
+    stagger:0.1
   }
 )
 
-
-tl.from(".full h4",
+gsap.from("h1 .b",
   {
-    x: 150,
-    duration: 0.4,
-    stagger: 0.2,
-    opacity: 0
+    y:70,
+    duration:0.6,
+    delay:0.4,
+    stagger:-0.1
   }
 )
-
-tl.from(".full i",
-  {
-    opacity: 0,
-    duration: 0.5
-  }
-)
-
-tl.pause();
-
-menu.addEventListener("click", function () {
-  tl.play();
-})
-
-close.addEventListener("click", function () {
-  tl.reverse();
-})
-
-
-
