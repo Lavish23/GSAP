c8302744696d7cef033f83956f6a6cdb2155767f
Lavish Sharma <sharmalavish375@gmail.com>
2024-06-12 15:56:35 +0530

    lec-6/completed

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-var tl=gsap.timeline();
-var full = document.querySelector(".full");
-var home = document.querySelector(".nav i");
-var close = document.querySelector(".full i");
-
-
-  tl.to(full,{
-    right:0,
-  })
-
-
-  tl.from(".full h4",
-    {
-      x:150,
-      opacity:0,
-      duration:0.4,
-      stagger:0.2
-    }
-  )
-
-  tl.pause();
-  home.addEventListener("click",function()
-  {
-    tl.play();
-  })
-
-  close.addEventListener("click",function()
+window.addEventListener("wheel",function(dets)
 {
-  tl.reverse();
+  if(dets.deltaY>0)
+    {
+      gsap.to(".marque",{
+        transform:'translateX(-200%)',
+        duration:3,
+        repeat:-1,
+        ease:"none"
+      })
+
+      gsap.to(".marque img",
+        {
+          rotate:"180",
+        }
+      )
+    }
+
+    else
+    {
+      gsap.to(".marque",{
+        transform:'translateX(0%)',
+        duration:3,
+        repeat:-1,
+        ease:"none"
+      })
+      gsap.to(".marque img",
+      {
+        rotate:"0",
+      }
+    )
+
+    }
 })
-
-
-
